e4fc0215516a588c049ff277e5690ddd6df33eb8
Tagurx <zseimei0315@gmail.com>
2025-09-15 11:26:43 +0800

    Update nexus-setup.sh

--- nexus-setup.sh
+++ nexus-setup.sh
@@ -12,8 +12,8 @@ sudo apt install build-essential pkg-config libssl-dev git-all -y
 # Install Nexus CLI
 curl https://cli.nexus.xyz/ | sh
 
-# Source .bashrc to load nexus-cli into PATH
-source /root/.bashrc
+# Manually add Nexus CLI to PATH for this script's environment
+export PATH="$HOME/.nexus/bin:$PATH"
 
 # Register user and node
 nexus-cli register-user --wallet-address 0x5085243f33dF36719d2B2C50D67E1586DE000e88
