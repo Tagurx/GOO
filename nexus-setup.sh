9ef3130cbde0efba0c0d22cf8abab5665aeeb35a
Tagurx <zseimei0315@gmail.com>
2025-09-15 11:33:07 +0800

    Update nexus-setup.sh

--- nexus-setup.sh
+++ nexus-setup.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-
-# Exit immediately if a command exits with a non-zero status
 set -e
 
 # Update and upgrade system
 sudo apt update && sudo apt upgrade -y
 
-# Install required packages
-sudo apt install build-essential pkg-config libssl-dev git-all -y
+# Avoid config prompts during apt installs
+echo 'Dpkg::Options {
+   "--force-confdef";
+   "--force-confold";
+};' | sudo tee /etc/apt/apt.conf.d/local-keep-conf > /dev/null
 
-# Install Nexus CLI
-curl https://cli.nexus.xyz/ | sh
+# Install required packages silently
+sudo DEBIAN_FRONTEND=noninteractive apt install build-essential pkg-config libssl-dev git-all -y
+
+curl -s https://cli.nexus.xyz/ | sh <<EOF
+y
+EOF
+
 
 # Manually add Nexus CLI to PATH for this script's environment
 export PATH="$HOME/.nexus/bin:$PATH"
